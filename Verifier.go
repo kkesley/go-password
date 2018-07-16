e4f0176a0e86c6d90e84d2a8657910e91452dc03
Kendrick Kesley <3642811@student.rmit.edu.au>
2018-07-16 10:50:12 +1000

    validate decoded bytes length

--- Verifier.go
+++ Verifier.go
@@ -12,6 +12,9 @@ func Verifier(password string, hash string, config Config) bool {
 	sDec, _ := base64.StdEncoding.DecodeString(hash)
 	saltBytes := readUint32BE(sDec, 0)
 	iterations := readUint32BE(sDec, 4)
+	if uint32(len(sDec)) < saltBytes+8 {
+		return false
+	}
 	salt := sDec[8 : saltBytes+8]
 	hashBytes := sDec[8+saltBytes:]
 
